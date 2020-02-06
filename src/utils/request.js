f6075e783c876113a6f32487e919889b0c24c125
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-02-06 14:51:29 +0800

    清理目录；normalize.css；rem适配；px转rem；封装axios；大数字类型

---
+++ src/utils/request.js
@@ -0,0 +1,23 @@
+// 封装axios请求模块
+import axios from 'axios'
+import jsonBig from 'json-bigint' // 解决大数字类型
+// axios.create 方法：复制一个 axios
+const request = axios.create({
+  baseUrl: 'http://ttapi.research.itcast.cn/'
+})
+// 解决大数字
+// axios 开放了自定义转换后端返回数据的 API
+// data 就是后端返回的原始数据
+request.defaults.transformResponse = [function (data) {
+  try {
+    // 它会把大数字转为一个对象，我们在使用的时候把对象.toString() 就得到字符串形式的 id 了
+    // 如果转换成功则返回成功的结果给请求使用
+    // 如果转换失败则进入 catch，返回一个空对象
+    return jsonBig.parse(data)
+  } catch (err) {
+    console.log('转换失败', err)
+    return {}
+  }
+}]
+
+export default request
