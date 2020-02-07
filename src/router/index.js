0508a8610c2a169040ae07b585e6572266dd601d
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-02-07 15:37:11 +0800

    tab-bar创建和4个分页面的配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,45 @@
 // 配置路由
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '@/views/login' // 引入登录页面
+// import login from '@/views/login' // 引入登录页面
 
 Vue.use(VueRouter)
 
 const routes = [{
   path: '/login', // 登录页面
-  component: Login
+  name: 'login',
+  component: () => ('@/views/login')
+}, {
+  path: '/',
+  name: 'tab-var',
+  component: () =>
+        import ('@/views/tab-bar'),
+  // 二级路由
+  children: [{
+    path: '', // 默认子路由
+    name: 'home',
+    component: () =>
+                import ('@/views/home')
+  },
+  {
+    path: 'qa',
+    name: 'qa',
+    component: () =>
+                import ('@/views/qa')
+  },
+  {
+    path: 'video',
+    name: 'video',
+    component: () =>
+                import ('@/views/video')
+  },
+  {
+    path: 'my',
+    name: 'my',
+    component: () =>
+                import ('@/views/my')
+  }
+  ]
 }]
 
 const router = new VueRouter({
