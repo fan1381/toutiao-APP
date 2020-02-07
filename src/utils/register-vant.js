d24278fc1920a3512021e21b33850c3315f76389
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-02-07 15:54:48 +0800

    处理是否登录的展示状态

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,21 +1,55 @@
 import Vue from 'vue'
-// 加载vant
+
+// 加载
 import {
   Button,
   Cell,
   CellGroup,
+  Icon,
   NavBar,
   Field,
-  Icon,
   Toast,
-  CountDown
+  CountDown,
+  Tabbar,
+  TabbarItem,
+  Image,
+  Grid,
+  GridItem,
+  Dialog,
+  List,
+  Tab,
+  Tabs,
+  PullRefresh,
+  Popup,
+  Search,
+  Loading,
+  ImagePreview,
+  ActionSheet,
+  DatetimePicker
 } from 'vant'
+
+// 注册
 Vue.use(Button)
   .use(Cell)
   .use(CellGroup)
+  .use(Icon)
   .use(NavBar)
   .use(Field)
-  .use(Icon)
   .use(Toast)
-
   .use(CountDown)
+  .use(Tabbar)
+  .use(TabbarItem)
+  .use(Image)
+  .use(Grid)
+  .use(GridItem)
+  .use(Dialog)
+  .use(List)
+  .use(Tab)
+  .use(Tabs)
+  .use(PullRefresh)
+  .use(Popup)
+  .use(Search)
+  .use(Loading)
+  .use(ImagePreview)
+  .use(ActionSheet)
+  .use(DatetimePicker)
