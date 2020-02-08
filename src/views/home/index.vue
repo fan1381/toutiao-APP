e1432f1d472d66872f51f8869f415337ccd9a328
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-02-08 14:24:11 +0800

    首页结构，list列表，上拉加载，下拉刷新

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar title="首页" />
+    <!-- /导航栏 -->
+
+    <!-- 频道列表 -->
+    <van-tabs v-model="active">
+      <van-tab title="标签 1">
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+            <van-cell v-for="item in list" :key="item" :title="item" />
+          </van-list>
+        </van-pull-refresh>
+      </van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <!-- /频道列表 -->
+  </div>
 </template>
 
 <script>
@@ -8,14 +27,70 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      active: 0, // 控制标签页的激活项，用v-model绑定
+      list: [],
+      loading: false,
+      finished: false,
+      isLoading: false
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    // 上划加载 list列表
+    onLoad () {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 1000)
+    },
+    // 下拉刷新
+    onRefresh () {
+      setTimeout(() => {
+        this.$toast('刷新成功')
+        this.isLoading = false
+      }, 500)
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.home-container {
+  .nav-bar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 15px;
+    height: 46px;
+    background-color: #3196fa;
+    z-index: 1;
+    .logo {
+      // background: url("./logo-light.png") no-repeat;
+      background-size: cover;
+      width: 100px;
+      height: 30px;
+    }
+    .search-btn {
+      background-color: #5babfb;
+      width: 50%;
+    }
+  }
+}
+</style>
