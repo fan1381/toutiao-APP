b3b9f562ed40ba41c3b6a69bbeaca7d2dbdd805b
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-02-10 22:52:08 +0800

    文章详情，关注状态，解析token

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { getItem, setItem } from '@/utils/storage'
+import decodeJwt from 'jwt-decode' // 解析token
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    // 登录用户，一个对象，包含 token 信息, null里面是token，refresh_token
+    // 登录用户，一个对象，包含 token 信息, null里面是token，refresh_token,用户信息等
     user: getItem('user') // vuex里面存的是对象
   },
   mutations: {
     setUser (state, data) {
+      if (data && data.token) { // 如果data有效并且有token，再去解析
+        data.id = decodeJwt(data.token).user_id
+      }
       state.user = data
       // 为了防止数据丢失，还要把数据放到本地储存持久化
       setItem('user', state.user) // 本地储存存的要是字符串
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -34,7 +34,7 @@
             <p class="time">{{article.pubdate}}</p>
           </div>
         </div>
-        <van-button class="follow-btn" type="info" size="small" round>+ 关注</van-button>
+        <van-button v-if="$store.state.user||article.aut_id!==当前登录用户id" class="follow-btn" type="info" size="small" round>{{ article.is_followed?'以关注':'关注' }}</van-button>
       </div>
       <div class="markdown-body" v-html="article.content">
       </div>
