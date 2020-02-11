b9cb62a4400afd18cb900400407f1ccabbd7b73b
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-02-11 16:13:38 +0800

    文章评论，格式化评论日期

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import 'normalize.css'
 import 'amfe-flexible' // rem 适配
 import './utils/register-vant' // 加载注册vant组件
 import './utils/validation.js' // 表单验证插件
+import './utils/datetime' // 格式化日期时间库
+
 Vue.config.productionTip = false
 Vue.use(Vant)
 
